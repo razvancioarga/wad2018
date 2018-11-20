fdbea64e9d202911432cc4cd3fe2f997764548bb
Razvan-Dorel CIOARGA <razvan.cioarga@gmail.com>
2018-11-20 18:32:38 +0200

    Initial creation of git repo on my laptop

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+	
+	<body>
+		<form method="POST" action="login.php">
+			<label for="username">Username:</label><input type="text" name="username" id="username" value="" /><br />
+			<label for="password">Password:</label>
+			<input type="password" name="password" id="password" value="" /><br />
+			<input type="submit" value="Login" />
+		</form>
+	</body>
+</html>
+<?php
